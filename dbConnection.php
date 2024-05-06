0d1c381951641a20241e58358ff38f9f4357b25d
Purshottam Jain <purshotamjaindelhi11@gmail.com>
2024-05-06 19:08:42 +0530

    php

---
+++ dbConnection.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = 'sql210.infinityfree.com';
+$databaseName = 'test';
+$databaseUsername = 'if0_36495598';
+$databasePassword = 'UrcfsV56on';
+
+// Open a new connection to the MySQL server
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
